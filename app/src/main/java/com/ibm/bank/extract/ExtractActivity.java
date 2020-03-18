aa14502c5b32e6cecb9fab9e5755d1aa13a318a6
Marcos Walker Silva Correia <marquinhos_thuglife@hotmail.com>
2020-03-18 03:54:44 -0300

    create extract screen layout

---
+++ app/src/main/java/com/ibm/bank/extract/ExtractActivity.java
@@ -0,0 +1,16 @@
+package com.ibm.bank.extract;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ibm.bank.R;
+
+public class ExtractActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extract);
+    }
+}
--- app/src/main/java/com/ibm/bank/login/LoginActivity.java
+++ app/src/main/java/com/ibm/bank/login/LoginActivity.java
@@ -2,15 +2,21 @@ package com.ibm.bank.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ibm.bank.R;
+import com.ibm.bank.extract.ExtractActivity;
 
 interface LoginActivityInput {
     void displayLoginData(LoginViewModel viewModel);
 }
 
-public class LoginActivity extends AppCompatActivity implements LoginActivityInput {
+public class LoginActivity extends AppCompatActivity implements LoginActivityInput, View.OnClickListener {
+
+    private Button btnLogin;
 
     LoginInteractorInput output;
     LoginRouter router;
@@ -19,7 +25,7 @@ public class LoginActivity extends AppCompatActivity implements LoginActivityInp
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        bindViews();
+        bindViews();
 
         LoginConfigurator.INSTANCE.configure(this);
         LoginRequest loginRequest = new LoginRequest();
@@ -30,5 +36,16 @@ public class LoginActivity extends AppCompatActivity implements LoginActivityInp
     @Override
     public void displayLoginData(LoginViewModel viewModel) {
 
+    }
+
+    private void bindViews() {
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ExtractActivity.class);
+        startActivity(intent);
     }
 }
