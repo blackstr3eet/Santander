2ffc81445dcfd391bb386c3794503fa913bb5c29
Marcos Walker Silva Correia <marquinhos_thuglife@hotmail.com>
2020-03-25 03:31:22 -0300

    refactor code

--- app/src/main/java/com/ibm/bank/util/Validation.java
+++ app/src/main/java/com/ibm/bank/util/Validation.java
@@ -5,7 +5,7 @@ public class Validation {
     private static final String LOGIN_USER_REGEX = "^(\\d{11}$)|((\\D+)([@])((.*\\.com)|(.*\\.com.br)))$";
     private static final String LOGIN_PASSWORD_REGEX = "((?=.*[0-9])|(?=.*[a-z]))(?=.*[A-Z])(?=.*[$*&@#])[a-z0-9A-Z$*&@#]{3,}";
 
-    public static  boolean checkUserLogin(String user) {
+    public static boolean checkUserLogin(String user) {
         return user.matches(LOGIN_USER_REGEX);
     }
 
